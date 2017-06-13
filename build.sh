9522a5633c9c41edb8d6820ff13cc4a6c163b295
SueChaplain <sue_chaplain@uk.ibm.com>
2017-06-13 10:46:35 +0100

    Create build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 
 sudo docker build -t mkdocs_build -f ./Dockerfile.template .
-sudo docker run -v $PWD:/docs -w /docs -it --rm mkdocs_build mkdocs --version
+sudo docker run mkdocs_build ls -l
+#sudo docker run -v $PWD:/docs -w /docs -it --rm mkdocs_build mkdocs --version
