05bcc9ae123bf90fd7c11a9ab504c4ed343bf076
Sue Chaplain <sue_chaplain@uk.ibm.com>
2017-06-19 12:06:16 +0100

    Rename Dockerfile.template to Dockerfile and update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 
-sudo docker build -t mkdocs_build_env -f ./Dockerfile.template .
+sudo docker build -t mkdocs_build_env -f ./Dockerfile .
 #sudo docker run -v $PWD:/docs -w /docs -it --rm mkdocs_build_env mkdocs --version
